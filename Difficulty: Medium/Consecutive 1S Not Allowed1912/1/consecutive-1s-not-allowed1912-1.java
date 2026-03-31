0201200953698ff6e261d84e5cb01f273210c56e
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2026-04-01 02:16:23 +0530

    Update consecutive-1s-not-allowed1912-1.java

--- Difficulty: Medium/Consecutive 1S Not Allowed1912/1/consecutive-1s-not-allowed1912-1.java
+++ Difficulty: Medium/Consecutive 1S Not Allowed1912/1/consecutive-1s-not-allowed1912-1.java
@@ -1,3 +1,18 @@
 class Solution {
+    int countStrings(int n) {
 
+        if (n == 1) return 2;
+        if (n == 2) return 3;
+
+        long prev2 = 2; // dp[1]
+        long prev1 = 3; // dp[2]
+
+        for (int i = 3; i <= n; i++) {
+            long curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return (int) prev1;
+    }
 }
