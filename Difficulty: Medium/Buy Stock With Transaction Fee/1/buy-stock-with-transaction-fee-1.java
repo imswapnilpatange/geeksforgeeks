5568dc0976e0644d2484e3d3a230901efc230133
imswapnilpatange <121827440+imswapnilpatange@users.noreply.github.com>
2026-03-31 13:32:42 +0530

    Update buy-stock-with-transaction-fee-1.java

--- Difficulty: Medium/Buy Stock With Transaction Fee/1/buy-stock-with-transaction-fee-1.java
+++ Difficulty: Medium/Buy Stock With Transaction Fee/1/buy-stock-with-transaction-fee-1.java
@@ -1,3 +1,15 @@
 class Solution {
+    public int maxProfit(int[] arr, int k) {
+        int noStock = 0;
+        int inHand = -arr[0];
 
+        for (int i = 1; i < arr.length; i++) {
+            int temp = noStock;
+
+            noStock = Math.max(noStock, inHand + arr[i] - k);
+            inHand = Math.max(inHand, temp - arr[i]);
+        }
+
+        return noStock;
+    }
 }
